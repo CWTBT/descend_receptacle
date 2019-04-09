28cc4c813015c90b290ba2f5dbd3bc1859b8f345
cwtbt <lw03232@gmail.com>
2019-04-09 15:58:25 -0500

    templates working

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,32 @@
 
 
 #[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+extern crate tera;
+
+use rocket_contrib::templates::Template;
+use tera::Context;
 
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+    .mount("/", routes![index])
+    .attach(Template::fairing())
+    .launch();
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/?<file_name>")]
+fn index(file_name: Option<String>) -> Template {
+    let mut context = Context::new();
+
+    match file_name {
+        None => context.insert("file_contents", "no file found"),
+        Some(file_name) =>
+        {
+            context.insert("file_contents", &file_name);
+            println!("it worked!");
+        }
+    }
+
+    Template::render("layout", &context)
 }
