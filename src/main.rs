40145b34e93b92c56683cd9479b808ca58932f6a
cwtbt <lw03232@gmail.com>
2019-04-10 18:12:38 -0500

    implemented MultiPartData parsing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@
 #[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 extern crate tera;
+extern crate rocket_multipart_form_data;
 
+use rocket::Data;
+use rocket_multipart_form_data::{MultipartFormData, MultipartFormDataOptions, MultipartFormDataField, FileField};
+use rocket::http::ContentType;
 use rocket_contrib::templates::Template;
 use tera::Context;
 
@@ -23,18 +27,27 @@ fn index() -> Template {
     Template::render("layout", &context)
 }
 
-#[post("/?<file_name>")]
-fn file_up(file_name: Option<String>) -> Template {
+#[post("/", data = "<data>")]
+fn file_up(content_type: &ContentType, data: Data) -> Template {
     let mut context = Context::new();
 
-    match file_name {
-        None => context.insert("file_contents", "no file found"),
-        Some(file_name) =>
-        {
-            context.insert("file_contents", &file_name);
-            println!("it worked!");
+    let mut options = MultipartFormDataOptions::new();
+    options.allowed_fields.push(MultipartFormDataField::file("target_file"));
+
+    let multipart_form_data = MultipartFormData::parse(content_type, data, options).unwrap();
+
+    let up_file = multipart_form_data.files.get(&"target_file".to_string());
+
+    if let Some(up_file) = up_file {
+        match up_file {
+            FileField::Single(file) => {
+                let _file_path = &file.path;
+            }
+
+            FileField::Multiple(_files) => {}
         }
     }
 
+    context.insert("file_contents", "Upload complete.");
     Template::render("layout", &context)
 }
