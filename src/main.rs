38af3ebf8d430024a9c059b41255ba6318cb4cf2
cwtbt <lw03232@gmail.com>
2019-04-29 01:48:18 -0500

    size limit increased, some documentation

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ fn main() {
     .launch();
 }
 
+// Reads contents of the RECEPTACLE to a vector.
 fn get_folder_contents() -> Vec<String> {
     let mut files = Vec::new();
 
@@ -38,12 +39,26 @@ fn get_folder_contents() -> Vec<String> {
     files
 }
 
+// Takes a context as a parameter. Inserts contents of the RECEPTACLE
+// into said context and returns it.
 fn pass_folder_contents(context: &mut Context) {
     let contents_vec = get_folder_contents();
     let contents_array = contents_vec.as_slice();
     context.insert("folder_contents", contents_array);
 }
 
+// Saves a file to the RECEPTACLE
+fn save_file(file_name: String, contents: &[u8]) {
+    let path = format!("/home/cwtbt/Documents/RustProjects/descend_receptacle/Receptacle/{}", file_name);
+
+    let mut saved_file = File::create(path).unwrap();
+    match saved_file.write_all(contents) {
+        Ok(_) => {},
+        Err(e) => println!("Failed writing: {}", e)
+    }
+}
+
+// Initializes the base page.
 #[get("/")]
 fn index() -> Template {
     let mut context = Context::new();
@@ -52,6 +67,8 @@ fn index() -> Template {
     Template::render("layout", &context)
 }
 
+// Updates only the table containing RECEPTACLE contents. This is primarily used with
+// the jQuery interval requests so the entire template doesn't have to be reloaded.
 #[get("/update")]
 fn update_contents() -> String {
     let contents = get_folder_contents();
@@ -90,7 +107,7 @@ fn file_del(file_name: String) -> Redirect {
 #[post("/", data = "<data>")]
 fn file_up(content_type: &ContentType, data: Data) -> Redirect {
     let mut options = MultipartFormDataOptions::new();
-    options.allowed_fields.push(MultipartFormDataField::raw("file"));
+    options.allowed_fields.push(MultipartFormDataField::raw("file").size_limit(2000000));
     let mut multipart_form_data = match MultipartFormData::parse(content_type, data, options) {
         Ok(multipart_form_data) => multipart_form_data,
         Err(err) => match err {
@@ -114,14 +131,4 @@ fn file_up(content_type: &ContentType, data: Data) -> Redirect {
     }
 
     Redirect::to("/")
-}
-
-fn save_file(file_name: String, contents: &[u8]) {
-    let path = format!("/home/cwtbt/Documents/RustProjects/descend_receptacle/Receptacle/{}", file_name);
-
-    let mut saved_file = File::create(path).unwrap();
-    match saved_file.write_all(contents) {
-        Ok(_) => {},
-        Err(e) => println!("Failed writing: {}", e)
-    }
 }
